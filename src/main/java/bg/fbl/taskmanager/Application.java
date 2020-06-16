1c052dd6b2d75108965f5ba7d59395cd0d2879a4
Boico Boicov <bboicov@fbl.bg>
2020-06-16 10:59:50 +0300

    Create project structure

---
+++ src/main/java/bg/fbl/taskmanager/Application.java
@@ -0,0 +1,10 @@
+package bg.fbl.taskmanager;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
