e1e8f0485ae660892783707e6c163696ab14b98d
Boico Boicov <bboicov@fbl.bg>
2020-06-17 10:47:21 +0300

    Add ProcessService

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# task-manager
-Task Manager implementation
+# Task-manager
+This component represents task manager. It allows users to add, list and kill processes.
+
+## Implementation
+Task manager is build on the top of popular light-weight and powerful Micronaut framework(https://micronaut.io).
+It is Spring and VertX alternative intended to quickly put together REST microservices.
+
+### Building blocks
+#### Priority
+The component is supporting LOW, MEDIUM, HIGH priority.
+
+#### OSProcess
+Each process contains three fields - unique Id(UUID in contrast to Linux based Ids), priority and creation time.
+
+#### ProcessFactory
+Factory which is used to create new processes with predefined priority.
+
+#### ProcessService
+Singleton service responsible for keeping track and managing processes.
+
+#### TaskManagerApi
+Endpoint controller which serve incoming requests.
+
+### Multithreading
+All entry endpoint methods of ProcessService are synchronized. Private methods are not because could not be called directly.
+There are numerous improvements available including using concurrent structures like ConcurrentHashMap, implement 
+more granular locking with ReentrantLock(https://winterbe.com/posts/2015/04/30/java8-concurrency-tutorial-synchronized-locks-examples/),
+combine read-shared and write-only locks, etc. 
+
+### Reactive processing 
+Micronaut supports reactive processing out of the box with RXJava. We can easily migrate the controller to the one.
+This will improve underlying resource usage(threads) and could improve simultaneous processing.
+
+## How to build the application
+
+## Running locally
+
+## Deploy to cloud
--- src/main/java/bg/fbl/taskmanager/service/ProcessService.java
+++ src/main/java/bg/fbl/taskmanager/service/ProcessService.java
@@ -1,4 +1,230 @@
 package bg.fbl.taskmanager.service;
 
+import bg.fbl.taskmanager.factory.ProcessFactory;
+import bg.fbl.taskmanager.model.OSProcess;
+import bg.fbl.taskmanager.model.Priority;
+
+import javax.inject.Singleton;
+import java.time.Instant;
+import java.util.*;
+
+@Singleton
 public class ProcessService {
+
+    static final private int MAX_PROCESSES = 100;
+
+    final private ProcessFactory processFactory;
+
+    HashMap<UUID, OSProcess> processPool = new HashMap<>();
+
+    // This could be organized as nested structures in order to support more than 3 priorities.
+    // For example map of ArrayDeques
+    ArrayDeque<UUID> lowPriorityProcesses = new ArrayDeque<>();
+    ArrayDeque<UUID> mediumPriorityProcesses = new ArrayDeque<>();
+    ArrayDeque<UUID> highPriorityProcesses = new ArrayDeque<>();
+
+    public ProcessService(ProcessFactory processFactory) {
+        this.processFactory = processFactory;
+    }
+
+    private UUID create(Priority priority) {
+
+        OSProcess process = processFactory.createProcess(priority);
+        processPool.put(process.getId(), process);
+        switch (priority) {
+            case LOW:
+                lowPriorityProcesses.addLast(process.getId());
+                break;
+            case MEDIUM:
+                mediumPriorityProcesses.addLast(process.getId());
+                break;
+            case HIGH:
+                highPriorityProcesses.addLast(process.getId());
+                break;
+        }
+
+        // start the process
+        process.run();
+
+        return process.getId();
+    }
+
+    private Instant getInstantOrDefault(UUID uuid) {
+        if (uuid != null) {
+            return processPool.get(uuid).getCreationTime();
+        }
+
+        return Instant.MAX;
+    }
+
+    private void remove(UUID uuid) {
+
+        OSProcess process = processPool.remove(uuid);
+        // we are not making validation if process will stop gracefully
+        // we could force kill if process is still running after certain timeout
+        process.kill();
+    }
+
+    private void deleteOldest() {
+
+        // We are sure that least one ArrayDeque is not empty(therefore peekFirst != null),
+        // because processPool.size() == MAX_PROCESSES
+        // Therefore will always find real oldest value.
+        Instant lowItem = getInstantOrDefault(lowPriorityProcesses.peekFirst());
+        Instant mediumItem = getInstantOrDefault(mediumPriorityProcesses.peekFirst());
+        Instant highItem = getInstantOrDefault(highPriorityProcesses.peekFirst());
+
+        UUID removedId;
+        if (lowItem.compareTo(mediumItem) < 0) {
+            // lowItem is smaller
+            if (lowItem.compareTo(highItem) < 0) {
+                // lowItem is the smallest
+                removedId = lowPriorityProcesses.pollFirst();
+            } else {
+                // highItem is the smallest
+                removedId = highPriorityProcesses.pollFirst();
+            }
+        } else {
+            // mediumItem is smaller
+            if (mediumItem.compareTo(highItem) < 0) {
+                // mediumItem is the smallest
+                removedId = mediumPriorityProcesses.pollFirst();
+            } else {
+                // highItem is the smallest
+                removedId = highPriorityProcesses.pollFirst();
+            }
+        }
+
+        remove(removedId);
+    }
+
+    synchronized public UUID addProcess(Priority priority) {
+
+        if (processPool.size() < MAX_PROCESSES) {
+            // there is free capacity for new process
+            return create(priority);
+        }
+
+        return null;
+    }
+
+    synchronized public UUID addProcessFIFO(Priority priority) {
+
+        if (processPool.size() == MAX_PROCESSES) {
+            deleteOldest();
+        }
+
+        return create(priority);
+    }
+
+    synchronized public UUID addProcessPriority(Priority priority) {
+
+        if (processPool.size() < MAX_PROCESSES) {
+            return create(priority);
+        }
+
+        // We can't add process based on the requirements and skip it
+        if (priority == Priority.LOW) return null;
+
+        if (lowPriorityProcesses.size() > 0) {
+            UUID removedId = lowPriorityProcesses.pollFirst();
+            remove(removedId);
+            return create(priority);
+        } else if (priority == Priority.HIGH && mediumPriorityProcesses.size() > 0) {
+            UUID removedId = mediumPriorityProcesses.pollFirst();
+            remove(removedId);
+            return create(priority);
+        }
+
+        return null;
+    }
+
+    synchronized public List<OSProcess> listProcesses() {
+
+        List<OSProcess> processList = new ArrayList<>();
+
+        // return all processes - order by priority and timestamp - ascending
+        Iterator iteratorL = lowPriorityProcesses.iterator();
+        while (iteratorL.hasNext()) {
+            processList.add(processPool.get(iteratorL.next()));
+        }
+
+        Iterator iteratorM = mediumPriorityProcesses.iterator();
+        while (iteratorM.hasNext()) {
+            processList.add(processPool.get(iteratorM.next()));
+        }
+
+        Iterator iteratorH = highPriorityProcesses.iterator();
+        while (iteratorH.hasNext()) {
+            processList.add(processPool.get(iteratorH.next()));
+        }
+
+        return processList;
+    }
+
+    synchronized public boolean killProcess(UUID processId) {
+
+        // first we search for the processId to validate is available in our pool
+        OSProcess process = processPool.get(processId);
+        if (process != null) {
+            UUID uuid = process.getId();
+            Priority priority = process.getPriority();
+            boolean result;
+            switch (priority) {
+                case LOW:
+                    result = lowPriorityProcesses.remove(uuid);
+                    break;
+                case MEDIUM:
+                    result = mediumPriorityProcesses.remove(uuid);
+                    break;
+                case HIGH:
+                    result = highPriorityProcesses.remove(uuid);
+                    break;
+                default:
+                    result = false;
+            }
+            if (result) {
+                remove(uuid);
+                return true;
+            }
+        }
+
+        // process Id not found
+        return false;
+    }
+
+    synchronized public void killAllWithPriority(Priority priority) {
+
+        ArrayDeque<UUID> targetDQ;
+        switch (priority) {
+            case LOW:
+                targetDQ = lowPriorityProcesses;
+                break;
+            case MEDIUM:
+                targetDQ = mediumPriorityProcesses;
+                break;
+            case HIGH:
+                targetDQ = highPriorityProcesses;
+                break;
+            default:
+                targetDQ = null;
+        }
+
+        if (targetDQ != null) {
+            Iterator iterator = targetDQ.iterator();
+            while (iterator.hasNext()) {
+                UUID uuid = (UUID) iterator.next();
+                remove(uuid);
+            }
+            targetDQ.clear();
+        }
+    }
+
+    synchronized public void killAll() {
+
+        // kill processes based on the priority
+        killAllWithPriority(Priority.LOW);
+        killAllWithPriority(Priority.MEDIUM);
+        killAllWithPriority(Priority.HIGH);
+    }
 }
